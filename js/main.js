5c15aa2616de445b8f981ee1f9660ce01617c91c
Rebecca Morris <RebeccaMorris@users.noreply.github.com>
2018-09-10 17:45:49 -0400

    Make responsive

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector('#menu');
+const main = document.querySelector('main');
+const drawer = document.querySelector('.nav');
+
+menu.addEventListener('click', function(e) {
+  drawer.classList.toggle('open');
+  // e.stopPropagation();
+});
+
+main.addEventListener('click', function(e) {
+  drawer.classList.remove('open');
+});
